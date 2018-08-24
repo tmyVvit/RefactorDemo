68d30eb18a258f10fe7009897559c7ad4c823073
Terry Teng <terry.teng@oocl.com>
2018-08-24 14:21:22 +0800

    [Terry] need to calculate retal's amount in both html statement and statement, add getAmount method to rental class

--- src/main/java/rentalstore/Customer.java
+++ src/main/java/rentalstore/Customer.java
@@ -25,26 +25,8 @@ public class Customer {
         Enumeration rentals = this.rentals.elements();
         String result = "Rental Record for " + getName() + "\n";
         while(rentals.hasMoreElements()){
-            double thisAmount =0;
             Rental each = (Rental) rentals.nextElement();
-
-            switch (each.getMovie().getPriceCode()){
-                case Movie.REGULAR:
-                    thisAmount += 2;
-                    if(each.getDayRented() > 2){
-                        thisAmount+=(each.getDayRented() - 2) * 1.5;
-                    }
-                    break;
-                case Movie.NEW_RELEASE:
-                    thisAmount+=each.getDayRented()*3;
-                    break;
-                    case Movie.CHILDRENS:
-                        thisAmount+=1.5;
-                        if(each.getDayRented() > 3){
-                            thisAmount += (each.getDayRented() -3)*1.5;
-                        }
-                        break;
-            }
+            double thisAmount =each.getAmount();
 
             //add frequent renter points
             frequentRenterPoints ++;
@@ -70,27 +52,8 @@ public class Customer {
         Enumeration rentals = this.rentals.elements();
         String result = "<H1>Rental Record for <EM>" + getName() + "</EM><H1><P>\n";
         while(rentals.hasMoreElements()){
-            double thisAmount =0;
             Rental each = (Rental) rentals.nextElement();
-
-            switch (each.getMovie().getPriceCode()){
-                case Movie.REGULAR:
-                    thisAmount += 2;
-                    if(each.getDayRented() > 2){
-                        thisAmount+=(each.getDayRented() - 2) * 1.5;
-                    }
-                    break;
-                case Movie.NEW_RELEASE:
-                    thisAmount+=each.getDayRented()*3;
-                    break;
-                case Movie.CHILDRENS:
-                    thisAmount+=1.5;
-                    if(each.getDayRented() > 3){
-                        thisAmount += (each.getDayRented() -3)*1.5;
-                    }
-                    break;
-            }
-
+            double thisAmount =each.getAmount();
             //add frequent renter points
             frequentRenterPoints ++;
             //add bonus for a two day new release rental
@@ -108,4 +71,6 @@ public class Customer {
         result += "You earned <EM>" + String.valueOf(frequentRenterPoints) + "</EM> frequent renter points";
         return result;
     }
+
+
 }
