986ac461b3e7468fcf3ffd26e96e51366c6eed66
Terry Teng <terry.teng@oocl.com>
2018-08-24 13:46:21 +0800

    [terry] should_return_correct_statement_given_customer_has_rent_one_new_release_movie_for_1_day sucess

--- src/test/java/rentalstore/CustomerTest.java
+++ src/test/java/rentalstore/CustomerTest.java
@@ -41,4 +41,18 @@ public class CustomerTest {
                 "Amount owed is 3.5\n" +
                 "You earned 1 frequent renter points", statement);
     }
+
+    @Test
+    public void should_return_correct_statement_given_customer_has_rent_one_new_release_movie_for_1_day() {
+        Movie newReleaseMovie = new Movie("Titanic", 1);
+        Rental oneDayRental = new Rental(newReleaseMovie, 1);
+        customer.addRental(oneDayRental);
+
+        String statement = customer.statement();
+
+        assertEquals("Rental Record for Terry\n" +
+                "\t" + newReleaseMovie.getTitle() + "\t3.0\n" +
+                "Amount owed is 3.0\n" +
+                "You earned 1 frequent renter points", statement);
+    }
 }
